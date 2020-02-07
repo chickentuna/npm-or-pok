54dc54802024c263b0d6c9f7e06d2a726beb1206
Julien Poulton <julien@codingame.com>
2020-02-07 23:06:41 +0100

    edit css

--- client/src/react/App.tsx
+++ client/src/react/App.tsx
@@ -11,8 +11,8 @@ function App () {
         <h1 className='App-header-title'>NPM OR POKEMON?</h1>
       </header>
       <div className='App-content'>
-        {/* <Router basename='/public/npm-or-pok'> */}
-        <Router basename='/'>
+        <Router basename='/public/npm-or-pok'>
+          {/* <Router basename='/'> */}
           <div>
             <Switch>
 
