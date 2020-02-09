f3de9b047cdf20edd905a41714e8c02a4f0579dd
Julien Poulton <julien@codingame.com>
2020-02-09 14:00:53 +0100

    fix css

--- client/src/react/Ranking.tsx
+++ client/src/react/Ranking.tsx
@@ -42,46 +42,46 @@ export class Ranking extends Component<Props, State> {
       <div className='ranking'>
         <div className='leaderboard'>
           <div className='items-wrapper'>
-            <div className='items'>
-
-              <div key={0} className='item'>
-                <div className='item-col item-rank'>
-                  <div className='item-col-content'>
+            <table className='items'>
+              <tbody>
+                <tr key={0} className='item'>
+                  <th className='item-col item-rank'>
+                    <div className='item-col-content'>
                     #
-                  </div>
-                </div>
-                <div className='item-col item-name'>
-                  <div className='item-col-content'>
+                    </div>
+                  </th>
+                  <th className='item-col item-name'>
+                    <div className='item-col-content'>
                     Name
-                  </div>
-                </div>
-                <div className='item-col item-score'>
-                  <div className='item-col-content'>
+                    </div>
+                  </th>
+                  <th className='item-col item-score'>
+                    <div className='item-col-content'>
                     Score
-                  </div>
-                </div>
-              </div>
-
-              {leaderboard && leaderboard.map(item => (
-                <div key={item.rank} className='item'>
-                  <div className='item-col item-rank'>
-                    <div className='item-col-content'>
-                      {item.rank}
                     </div>
-                  </div>
-                  <div className='item-col item-name'>
-                    <div className='item-col-content'>
-                      {item.name}
-                    </div>
-                  </div>
-                  <div className='item-col item-score'>
-                    <div className='item-col-content'>
-                      {item.score}
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
+                  </th>
+                </tr>
+                {leaderboard && leaderboard.map(item => (
+                  <tr key={item.rank} className='item'>
+                    <td className='item-col item-rank'>
+                      <div className='item-col-content'>
+                        {item.rank}
+                      </div>
+                    </td>
+                    <td className='item-col item-name'>
+                      <div className='item-col-content'>
+                        {item.name}
+                      </div>
+                    </td>
+                    <td className='item-col item-score'>
+                      <div className='item-col-content'>
+                        {item.score}
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
         <div className='home-link'>
